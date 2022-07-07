bad63ec997d130eac111e6c78cf871b0b663c1a8
JessicaM04 <jessica.mcnamara84@gmail.com>
2022-07-07 09:28:41 -0400

    change row color based on current time

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,28 @@
 var day = moment().format("dddd") + ", " + moment().format("MMMM Do"); 
+var time = moment().hour();
 var eventIdTracker = 0;
+var input = document.querySelectorAll("input");
+
+for (var i = 0; i < input.length; i++) {
+  console.log(input[i]);
+  console.log(input[i].id);
+  if (time == input[i].id) {
+    input[i].classList.add("present");
+  }
+  
+  if (time > input[i].id) {
+    input[i].classList.add("past");
+  }
+  
+  if (time < input[i].id) {
+    input[i].classList.add("future");  
+}
+};  
 
 function showCurrentDate() {
   var currentDayP = document.getElementById("currentDay");
   currentDayP.innerHTML = day;
 }
-
 
 function saveEvent() {
   var inputEvent = document.getElementById("inputEvent").value;
@@ -17,6 +34,10 @@ function saveEvent() {
   var timeDiv = document.querySelector("div").value;
   console.log(timeDiv);
  }
+
+
+
+
 
 
 showCurrentDate();
